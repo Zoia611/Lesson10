f6f01023686dc368c2d2ad42f85cd83b0769ade1
Zoia611 <117438266+Zoia611@users.noreply.github.com>
2022-11-23 13:09:07 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        class Corner
+        {
+            int gradus;
+            int minute;
+            int second;
+            public int Gradus
+            {
+                get
+                {
+                    return gradus;
+                }
+                set
+                {
+                    gradus = value;
+                }
+            }
+            public int Minute
+            {
+                get
+                {
+                    return minute;
+                }
+                set
+                {
+                    minute = value;
+                }
+            }
+            public int Seccond
+            {
+                get
+                {
+                    return second;
+                }
+                set
+                {
+                    second = value;
+                }
+            }
+
+            public Corner(int gradus, int min, int sec)
+            {
+                this.gradus = gradus;
+                this.minute = minute;
+                this.second = second;
+            }
+            public double ToRadians()
+            {
+                return gradus * Math.PI / 180 + minute * Math.PI / (180 * 60) + second * Math.PI / (180 * 3600);
+
+                static void Main(string[] args)
+                {
+                    Console.WriteLine("Введите угол с указанием градусов, минут, секунд (ввести 3 целочисленных значения)");
+                    int gradus = Convert.ToInt32(Console.ReadLine());
+                    int minute = Convert.ToInt32(Console.ReadLine());
+                    int second = Convert.ToInt32(Console.ReadLine());
+
+                    Corner corner = new Corner(gradus, minute, second);
+                    double radians = corner.ToRadians();
+                    Console.WriteLine(radians);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
